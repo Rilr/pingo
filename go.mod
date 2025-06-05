b5d55d894aada9488fb72efb217209076d76e447
Rilr <Jorgelschn@gmail.com>
2025-06-05 16:00:31 -0400

    init commit
    Needs to action if no ping is received.

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module jorgeschneider.com/code/pingo
+module pingo
 
 go 1.24.1
 
@@ -6,7 +6,7 @@ require github.com/prometheus-community/pro-bing v0.7.0
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sync v0.15.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	"fmt"
-	"github.com/prometheus-community/pro-bing"
+	"os"
+	"time"
+
+	p "github.com/prometheus-community/pro-bing"
 )
 
-pinger, err := probing.NewPinger("www.google.com")
-if err != nil {
-	panic(err)
+func main() {
+	address := "8.8.8.8"
+	interval := time.Minute
+
+	for {
+		success := false
+		for i := 0; i < 5; i++ {
+			pinger, err := p.NewPinger(address)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to create pinger: %v\n", err)
+				continue
+			}
+			pinger.SetPrivileged(true)
+			pinger.Count = 1
+			pinger.Timeout = time.Second * 2
+			err = pinger.Run()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Ping error: %v\n", err)
+				continue
+			}
+			stats := pinger.Statistics()
+			if stats.PacketsRecv > 0 {
+				success = true
+				fmt.Printf("Ping %d: reply from %s\n", i+1, address)
+			} else {
+				fmt.Printf("Ping %d: no reply from %s\n", i+1, address)
+			}
+			time.Sleep(2 * time.Second)
+		}
+		if success {
+			fmt.Printf("Result: At least one reply from %s in this minute.\n\n", address)
+		} else {
+			fmt.Printf("Result: No replies from %s in this minute.\n\n", address)
+		}
+		time.Sleep(interval - 10*time.Second) // 5 pings x 2s = 10s, so wait the rest of the minute
+	}
 }
-pinger.Count = 3
-err = pinger.Run() // Blocks until finished.
-if err != nil {
-	panic(err)
-}
-stats := pinger.Statistics()
