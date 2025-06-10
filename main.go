4c65356a47728105496d45dcccedbdb950523e09
Rilr <Jorgelschn@gmail.com>
2025-06-10 19:48:07 -0400

    Changed the main loop and added placeholders
    Move the logic of the main function around to better suit my needs, while creating place holders for future programs.

--- main.go
+++ main.go
@@ -8,40 +8,76 @@ import (
 	p "github.com/prometheus-community/pro-bing"
 )
 
+func BadPing() {
+	if TestISP() { // if the ISP responds, tunnel is probably down
+		CreateTicket()
+		TtyIntoHost()
+	} else {
+		AddtoLog() // if the ISP does not respond, we can't create a ticket nor get the tunnel restored until the ISP is back online
+		os.Exit(11)
+	}
+}
+
+func TestISP() bool {
+	AddtoLog()
+	return true
+}
+
+func CreateTicket() int{
+	AddtoLog()
+	ticketNumber := 123456 // Simulated ticket number
+	return ticketNumber
+}
+
+func TtyIntoHost() {
+	fmt.Println("This function is not used in the main program.")
+}
+
+func ElevateTicket(t int) error{
+	if t >= 0 {
+		fmt.Print(t)
+		return nil
+	} else {
+		return fmt.Errorf("invalid ticket number: %d", t)
+	}
+}
+
+func CloseTicket(t int) error{
+	if t >= 0 {
+		fmt.Print(t)
+		return nil
+	} else {
+		return fmt.Errorf("invalid ticket number: %d", t)
+	}
+}
+
+func AddtoLog() {
+	fmt.Println("This function is not used in the main program.")
+}
+
 func main() {
 	address := "8.8.8.8"
-	interval := time.Minute
+		for i := 0; i < 2; i++ {
+		pinger, err := p.NewPinger(address)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create pinger: %v\n", err)
+		}
+		pinger.SetPrivileged(true) // Allows to run without sudo
+		pinger.Count = 10
+		err = pinger.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ping error: %v\n", err)
+			continue
+		}
+		stats := pinger.Statistics()
+		switch{
+			case stats.PacketLoss >= 20:
+				fmt.Printf("%s: Ping to %s resulted in %f%% packet loss.\n", time.Now().Format("2006-01-02_15:04:05"), address, stats.PacketLoss)
 
-	for {
-		success := false
-		for i := 0; i < 5; i++ {
-			pinger, err := p.NewPinger(address)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to create pinger: %v\n", err)
-				continue
-			}
-			pinger.SetPrivileged(true)
-			pinger.Count = 1
-			pinger.Timeout = time.Second * 2
-			err = pinger.Run()
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Ping error: %v\n", err)
-				continue
-			}
-			stats := pinger.Statistics()
-			if stats.PacketsRecv > 0 {
-				success = true
-				fmt.Printf("Ping %d: reply from %s\n", i+1, address)
-			} else {
-				fmt.Printf("Ping %d: no reply from %s\n", i+1, address)
-			}
-			time.Sleep(2 * time.Second)
+			case stats.PacketLoss < 20 && stats.PacketLoss > 0:
+				fmt.Printf("Ping to %s resulted in %f%% packet loss.\n Average RTT: %s. Deviation: %s\n", address, stats.PacketLoss, stats.AvgRtt.String(), stats.StdDevRtt.String())
+			default:
+				fmt.Printf("Ping to %s resulted in zero packet loss.\n Average RTT: %s. Deviation: %s\n", address, stats.AvgRtt.String(), stats.StdDevRtt.String())
 		}
-		if success {
-			fmt.Printf("Result: At least one reply from %s in this minute.\n\n", address)
-		} else {
-			fmt.Printf("Result: No replies from %s in this minute.\n\n", address)
-		}
-		time.Sleep(interval - 10*time.Second) // 5 pings x 2s = 10s, so wait the rest of the minute
 	}
 }
